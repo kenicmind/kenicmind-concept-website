694290c85fc3681db27b07428767d52fd4c9c9b8
kenicmind <kenicmind@gmail.com>
2026-09-25 09:46:31 +0100

    Refresh JDC icon cache

--- public/jdc/sw.js
+++ public/jdc/sw.js
@@ -1,4 +1,4 @@
-const CACHE='jdc-v95';
+const CACHE='jdc-v95b';
 self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(['./','./manifest.webmanifest','./jdc-icon-180.png','./jdc-icon-512.jpg'])));self.skipWaiting();});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()));});
 self.addEventListener('fetch',e=>{if(e.request.method!=='GET')return;const u=new URL(e.request.url);if(u.origin!==self.location.origin)return;if(e.request.mode==='navigate'){e.respondWith(fetch(e.request,{cache:'no-store'}).catch(()=>caches.match('./')));}});
